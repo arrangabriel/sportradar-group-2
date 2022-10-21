9ca19fd039d31d5de48ebe766e3874db61ff7bfe
Arran <arrankg@gmail.com>
2022-10-21 16:52:42 +0200

    Add spring backend

---
+++ GlobeBackend/src/main/java/com/globeboys/GlobeBackend/GlobeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.globeboys.GlobeBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlobeBackendApplication.class, args);
+	}
+
+}
---
+++ GlobeBackend/src/test/java/com/globeboys/GlobeBackend/GlobeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.globeboys.GlobeBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlobeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
