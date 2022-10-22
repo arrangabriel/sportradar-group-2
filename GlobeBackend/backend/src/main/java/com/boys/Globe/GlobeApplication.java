e50b1e3c5cdd8fe359537b4cce7804f9d46861a7
Arran <arrankg@gmail.com>
2022-10-22 10:30:42 +0200

    Add backend

---
+++ GlobeBackend/backend/src/main/java/com/boys/Globe/GlobeApplication.java
@@ -0,0 +1,15 @@
+package com.boys.Globe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlobeApplication {
+
+	public GlobeApplication(){
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(GlobeApplication.class, args);
+	}
+
+}
