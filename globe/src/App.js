0610b24f5e64e13ec7643f925de16dd64b5de08c
Elias Borge <elias@secunor.no>
2022-10-22 12:16:45 +0200

    Finished map framework

--- globe/src/App.js
+++ globe/src/App.js
@@ -1,6 +1,19 @@
-// app.js
+/// app.js
 import React from "react";
 import Map from "./components/Map";
+
+// Set your mapbox access token here
+const MAPBOX_ACCESS_TOKEN =
+	"pk.eyJ1IjoiZWxpYXNibyIsImEiOiJjbDlqb3ZtZmgwb2RjNDFvZGpjYzM3NThsIn0.dsVLOFPMeZCI4TnSkGENNw";
+
+// Viewport settings
+const INITIAL_VIEW_STATE = {
+	longitude: 24,
+	latitude: 26,
+	zoom: 1.9,
+	pitch: 30,
+	bearing: 0,
+};
 
 // Data to be used by the LineLayer
 const data = [
@@ -11,5 +24,9 @@ const data = [
 ];
 
 export default function App() {
-	return <Map data={data} />;
+	return (
+		<div className="App">
+			<Map data={data} />
+		</div>
+	);
 }
--- globe/src/components/Map.js
+++ globe/src/components/Map.js
@@ -1,23 +1,26 @@
 import React, { useCallback, useRef, useState } from "react";
 
 import DeckGL from "@deck.gl/react";
-import "mapbox-gl/dist/mapbox-gl.css";
-import ReactMapGL from "react-map-gl";
 import { LineLayer } from "@deck.gl/layers";
+import { Map as MapboxMap } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+
 // Set your mapbox access token here
-const MAPBOX_ACCESS_TOKEN =
-	"pk.eyJ1IjoiZWxpYXNibyIsImEiOiJjbDlqb3ZtZmgwb2RjNDFvZGpjYzM3NThsIn0.dsVLOFPMeZCI4TnSkGENNw";
+const MAPBOX_ACCESS_TOKEN = process.env.REACT_APP_MAPBOX_ACCESS_TOKEN;
 
 // Viewport settings
 const INITIAL_VIEW_STATE = {
-	longitude: -122.41669,
-	latitude: 37.7853,
-	zoom: 13,
-	pitch: 0,
+	longitude: 24,
+	latitude: 26,
+	zoom: 1.9,
+	pitch: 30,
 	bearing: 0,
 };
 
-function Map(props) {
+// pk.eyJ1IjoiZWxpYXNibyIsImEiOiJjbDlqcjl2cGEwOHNpM25wbGpuOWt4M2J5In0.QTfDDzyvlaU5MBgkx3KqBA
+// export REACT_APP_MAPBOX_TOKEN={pk.eyJ1IjoiZWxpYXNibyIsImEiOiJjbDlqcjl2cGEwOHNpM25wbGpuOWt4M2J5In0.QTfDDzyvlaU5MBgkx3KqBA} && npm start
+
+export default function Map(props) {
 	const { data } = props;
 
 	const layers = [new LineLayer({ id: "line-layer", data })];
@@ -25,12 +28,13 @@ function Map(props) {
 	return (
 		<DeckGL
 			initialViewState={INITIAL_VIEW_STATE}
-			controller={true}
+			controller={false}
 			layers={layers}
 		>
-			<ReactMapGL mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN} />
+			<MapboxMap
+				mapboxAccessToken={MAPBOX_ACCESS_TOKEN}
+				mapStyle="mapbox://styles/eliasbo/cl9iuvkog00io14pa0zz74brt"
+			/>
 		</DeckGL>
 	);
 }
-
-export default Map;
