d26ea9807a2ccc32fc19a44929a2370784b09bac
Arran <arrankg@gmail.com>
2022-10-22 11:31:52 +0200

    Remove backend

--- GlobeBackend/sdk/sdk-test/src/test/java/com/sportradar/sdk/test/system/framework/common/SdkDataListener.java
+++
@@ -1,8 +0,0 @@
-package com.sportradar.sdk.test.system.framework.common;
-
-import com.sportradar.sdk.proto.dto.OutgoingMessage;
-
-public interface SdkDataListener {
-
-    public void sdkEntitySend(OutgoingMessage entity);
-}
